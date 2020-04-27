7500c17be46ae3a70fc03d974d88bc49f35addcc
Mohammed Amin <arta0613@gmail.com>
2020-04-27 17:30:38 -0400

    Populate Sandwich Details UI

--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,56 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich selectedSandwich = null;
+
+        try {
+            JSONObject selectedSandwichJsonObject = new JSONObject(json);
+            JSONObject sanwichNameJsonObject = selectedSandwichJsonObject.getJSONObject("name");
+
+            selectedSandwich = new Sandwich(
+                    getJSONString(sanwichNameJsonObject, "mainName"),
+                    convertJSONArrayToList(getJSONArray(sanwichNameJsonObject, "alsoKnownAs")),
+                    getJSONString(selectedSandwichJsonObject, "placeOfOrigin"),
+                    getJSONString(selectedSandwichJsonObject, "description"),
+                    getJSONString(selectedSandwichJsonObject, "image"),
+                    convertJSONArrayToList(getJSONArray(selectedSandwichJsonObject, "ingredients"))
+            );
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return selectedSandwich;
+    }
+
+    private static String getJSONString(JSONObject sandwichObject, String id) throws JSONException {
+        return sandwichObject.getString(id);
+    }
+
+    private static JSONArray getJSONArray(JSONObject sandwichObject, String id) throws JSONException {
+        return sandwichObject.getJSONArray(id);
+    }
+
+    private static List<String> convertJSONArrayToList(JSONArray jsonArray) {
+        List<String> jsonList = new ArrayList<>();
+
+        try {
+            for (int i = 0; i < jsonArray.length(); i++) {
+                jsonList.add(jsonArray.getString(i));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return jsonList;
     }
 }
